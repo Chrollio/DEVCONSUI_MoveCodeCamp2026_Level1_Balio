bd285b1e8fcc3e4be73bea32a8b93b9778de594e
Christian Balio <christianbalio04@example.com>
2026-03-28 16:16:03 +0800

    hey

--- portfolio_frontend/src/views/PortfolioView.tsx
+++ portfolio_frontend/src/views/PortfolioView.tsx
@@ -98,7 +98,7 @@ const PortfolioView = () => {
   // ==========================================================================
   // STATE MANAGEMENT
   // ==========================================================================
-  const objectId = "0xa3343391df96e28464499f4c209d51bf209c07392fdeea97bfeee59e7550f020";
+  const objectId = "0x16cfc38abdf230fc834f439f07f6774cf184c97c4f3c1d714ce70756574d0185";
   
   // Network state - default to testnet, can be changed if needed
   const [currentNetwork, setCurrentNetwork] = useState<"testnet" | "mainnet">("mainnet");
